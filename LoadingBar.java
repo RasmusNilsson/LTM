995c5c08fccee15d2022b47ce84a3642a24d48bf
Rasmus Mølck Nilsson <fudling@gmail.com>
2016-05-18 11:59:14 +0200

    Update LoadingBar.java

--- LoadingBar.java
+++ LoadingBar.java
@@ -3,11 +3,11 @@
  * It will print out a loading bar as the algorithm progresses.
  * 
  * Please ignore.
- * @author Rasmus Mølck Nilsson
+ * @author Rasmus MÃ¸lck Nilsson
  *
  */
 public class LoadingBar {
-	private static final int[] timeWeights = {50,1000,3500,2000,1000,300,200,50,30,20,15,10,6,5,4,3,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}; //{50,150,1300,2600,2000,750,350,200,100,50,25,12,6,5,4,3,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+	private static final int[] timeWeights = {50,200,1300,2000,1500,750,350,200,100,90,80,70,60,50,40,30,20,10,5,4,3,2,1,1,1,1,1,1,1,1,1,1,1,1,1}; //{50,150,1300,2600,2000,750,350,200,100,50,25,12,6,5,4,3,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 	private static final String numbers = "0       10        20        30        40        50        60        70        80        90       100\n|";
 	private static final int totalChars = 100;
 	private static int accChars;
